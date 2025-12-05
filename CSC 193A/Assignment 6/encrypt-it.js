156cf3f19937e95774aedee347f02663219332d6
TheMightyOctopus <46735357+TheMightyOctopus@users.noreply.github.com>
2025-12-05 04:09:15 -0800

    Completed Assignment 6. Also removed an empty line in fancifymytext.html.

--- CSC 193A/Assignment 6/encrypt-it.js
+++ CSC 193A/Assignment 6/encrypt-it.js
@@ -12,15 +12,86 @@
   window.addEventListener("load", init);
 
   /**
-   * TODO: Write a function comment using JSDoc.
+   * This function is called when the "load" event fires on the window, 
+   * signalling the HTML DOM has been constructed. 
+   * Here, we set up event handlers for the buttons on the page. 
    */
   function init() {
+
     // Note: In this function, we usually want to set up our event handlers
     // for UI elements on the page.
+    console.log("Window loaded!");
+
+    let encryptButton = document.getElementById("encrypt-it");
+    encryptButton.addEventListener("click", handleEncrypt);
+
+    let resetButton = document.getElementById("reset");
+    resetButton.addEventListener("click", handleReset);
+
   }
 
   // Add any other functions in this area (you should not implement your
   // entire program in the init function, for similar reasons that
   // you shouldn't write an entire Java program in the main method).
+
+  function handleEncrypt() {
+
+    console.log("Button clicked!");
+
+    let input = document.getElementById("input-text");
+    let text = input.value;
+
+    let cipher = document.getElementById("cipher-type");
+    let cipherType = cipher.value;
+
+    let result = document.getElementById("result");
+
+    if (cipherType === "shift") {
+
+      result.textContent = shiftCipher(text);
+
+    } else {
+
+      result.textContent = "Cipher not implemented yet!";
+
+    }
+
+  }
+
+  function handleReset() {
+
+    console.log("Reset clicked!");
+
+    let input = document.getElementById("input-text");
+    input.value = "";
+
+    let result = document.getElementById("result");
+    result.textContent = "";
+
+  }
+
+  function shiftCipher(text) {
+
+    text = text.toLowerCase();
+
+    for (let i = 0; i < text.length; i++) {
+
+      let char = text.charAt(i);
+
+      if (char >= 'a' && char <= 'z') {
+
+        let charCode = char.charCodeAt(0);
+        charCode = (charCode - 97 + 1) % 26 + 97;
+        text = text.slice(0, i) 
+             + String.fromCharCode(charCode) 
+             + text.slice(i + 1);
+
+      }
+
+    }
+
+    return text;
+
+  }
 
 })();
